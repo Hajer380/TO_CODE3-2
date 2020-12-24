a03391a6e94d985ce96e2b581977f5bf653bc9ab
HajerCHIKHA <72881004+Hajer380@users.noreply.github.com>
2020-12-25 00:38:45 +0100

    Update InfoStudent.php

--- WS-TD/InfoStudent.php
+++ WS-TD/InfoStudent.php
@@ -3,7 +3,7 @@ class InfoStudent {
 
 //function to be exposed must be public
 public function getInfoStudent($cin) {
-$info = array("CIN:".$cin , "Hatem", "Riahi","20/07/1996", 
+$info = array("CIN:".$cin , "chikha", "hajer","07/01/1997", 
 "Level: 3", "Result:reussi"); 
 return $info;
 }
